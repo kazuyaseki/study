bc09ce639d3b493c9aeff7c6239aafcc5f4f47d8
kazuya-seki <track613armory@gmail.com>
2018-06-23 13:41:44 +0900

    とりあえずタブを追加して見た

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
+  public state = {
+    name: '',
+    value: 0
+  };
+
+  public handleChange = (e: any, value: string) => {
+    this.setState({ value });
+  };
+
+  public handleChangeName = (e: any) => {
+    this.setState({ name: e.target.value });
+  };
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Tabs value={this.state.value} onChange={this.handleChange}>
+          <Tab label="トピック" />
+          <Tab label="日報" />
+          <Tab label="パイル" />
+        </Tabs>
+        <ul className="activities">
+          <li>
+            <TextField
+              id="name"
+              label="Name"
+              value={this.state.name}
+              onChange={this.handleChangeName}
+              margin="normal"
+            />
+          </li>
+        </ul>>
       </div>
     );
   }
